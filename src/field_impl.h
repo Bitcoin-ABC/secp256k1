935c58890e54219043e8c24282c32e7ea1c0d7d5
Pieter Wuille <pieter@wuille.net>
2023-05-11 04:42:09 -0400

    [secp256k1] Simplify secp256k1_fe_{impl_,}verify
    Summary:
    This concludes backport of [[https://github.com/bitcoin-core/secp256k1/pull/1066 | secp256k1#1066]]
    https://github.com/bitcoin-core/secp256k1/pull/1066/changes/7fc642fa25ad03ebd95cfe237b625dfb6dfdfa94
    Depends on D19872
    
    Test Plan: `ninja check-secp256k1`
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D19873

--- src/field_impl.h
+++ src/field_impl.h
@@ -167,12 +167,11 @@ static void secp256k1_fe_verify(const secp256k1_fe *a) { (void)a; }
 #else
 static void secp256k1_fe_verify(const secp256k1_fe *a) {
     /* Magnitude between 0 and 32. */
-    int r = (a->magnitude >= 0) & (a->magnitude <= 32);
+    VERIFY_CHECK((a->magnitude >= 0) && (a->magnitude <= 32));
     /* Normalized is 0 or 1. */
-    r &= (a->normalized == 0) | (a->normalized == 1);
+    VERIFY_CHECK((a->normalized == 0) || (a->normalized == 1));
     /* If normalized, magnitude must be 0 or 1. */
-    if (a->normalized) r &= (a->magnitude <= 1);
-    VERIFY_CHECK(r == 1);
+    if (a->normalized) VERIFY_CHECK(a->magnitude <= 1);
     /* Invoke implementation-specific checks. */
     secp256k1_fe_impl_verify(a);
 }
